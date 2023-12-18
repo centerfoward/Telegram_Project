dece94b7a46172fc751b1bc582ffb24cbde31688
centerfoward <jsu5438@naver.com>
2023-12-19 00:02:30 +0900

    feat:기능완성

---
+++ ex.py
@@ -0,0 +1,5 @@
+#6604358519:AAH_jKjPXeog9fRZCYsy1awvTmAocLEeb4I swmj봇 토큰
+import telegram
+
+token="6604358519:AAH_jKjPXeog9fRZCYsy1awvTmAocLEeb4I"
+async def start():
--- index.py
+++ index.py
@@ -5,10 +5,13 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 TOKEN = '6765317557:AAHvuR9iD52G2HaXabZcOBNzlk7vTVYyl2U'  # 텔레그램 봇 API
 async def start(update : Update, context: ContextTypes.DEFAULT_TYPE) :
-    send_msg = "환영합니다" #버튼 상단 메시지 
-    btn1 = InlineKeyboardButton(text = "1. 네이버",url="www.naver.com") # 버튼 설정
-    btn2 = InlineKeyboardButton(text = "2. 구글",url="www.gogle.com")
-    mu = InlineKeyboardMarkup(inline_keyboard = [[btn1, btn2]]) # 버튼 배치 설정
+    send_msg = "🎄Welcome to the vibrant world of Renewlabs🎄\n🎄          on our official Telegram channel!        🎄" #버튼 상단 메시지 
+    btn1 = InlineKeyboardButton(text = "Facebook🪴",url="https://www.facebook.com/renewlabs.official/") # 버튼 설정
+    btn2 = InlineKeyboardButton(text = "Instagram🌵",url="https://www.instagram.com/renewlabs.official/")
+    btn3 = InlineKeyboardButton(text="X🍃",url="https://twitter.com/renew_labs")
+    btn4 = InlineKeyboardButton(text="Discord🎋",url="https://discord.gg/DuVbZHEXpm")
+    btn5 = InlineKeyboardButton(text="Link Tree🌲",url="https://linktr.ee/renewlabs")
+    mu = InlineKeyboardMarkup(inline_keyboard = [[btn1, btn2],[btn3,btn4],[btn5]]) # 버튼 배치 설정
     await context.bot.send_message(chat_id=update._effective_chat.id, reply_markup=mu,text=send_msg) #비동기식 버튼 전송
 
 if __name__ == '__main__': # 메인 함수
